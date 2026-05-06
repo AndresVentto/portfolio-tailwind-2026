f0d8e7f7dbae8f740afc57939cff82fb4202697d
Andrés Vento <andresvento2004@gmail.com>
2026-05-06 13:43:39 -0400

    mejoras y pruebas

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ import { copyAr, renderContacto } from './copy.js';
 import { initContactForm } from './contact.js'; 
 import { openModal, closeModal } from './modalProyect.js'; 
 
-// Ejecución de funciones
+// Ejecución de las funciones
 menuFunciones();
 scrollHeaderFondo();
 copyAr();
@@ -15,7 +15,7 @@ initContactForm();
 openModal('modal-project-1');
 closeModal('modal-project-1');
 
-
+ 
 
 // Deshabilitar clik derecho
 document.addEventListener('contextmenu', event => event.preventDefault());
